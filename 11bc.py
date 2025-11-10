c88ef70be6ea519cebf421d306fe0987d49b7aaf
Axel0034 <palenaxe000@rocori.k12.mn.us>
2025-11-10 13:08:48 -0600

    q

---
+++ 11bc.py
@@ -0,0 +1,11 @@
+# Messages
+msg_b = "!$Y@o* u%$ #uaarreqee$ raa@m%$a!dz¨zidmn!ag#g!"
+msg_c = "$Y@o* u%$ #uaarreqee$ raa@m%$a!dz¨zidmn!ag#g!"
+
+# String Slicing
+decoded_b = msg_b[2:33]
+decoded_c = msg_c[26:11:-1]
+
+# Display results
+print("Decoded b:", decoded_b)
+print("Decoded c:", decoded_c)
